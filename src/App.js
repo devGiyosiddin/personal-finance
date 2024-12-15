328a11cf154dc74ac3c577a93fcd9204d4da5518
devGiyosiddin <devgiyosiddin@gmail.com>
2024-12-15 14:30:48 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1 className='title'>Personal Finance</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,3 @@
+const api = '6c54e1f37780c9f35bb9c009';
+export default api;
+// https://v6.exchangerate-api.com/v6/6c54e1f37780c9f35bb9c009/latest/USD
