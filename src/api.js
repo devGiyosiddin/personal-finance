570bdc93cbd5f9bd1198e6a14336f2f0860a3a5b
devGiyosiddin <devgiyosiddin@gmail.com>
2024-12-15 14:35:05 +0500

    Api key

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-const api = '6c54e1f37780c9f35bb9c009';
-export default api;
+const apiKey = '6c54e1f37780c9f35bb9c009';
+export default apiKey;
 // https://v6.exchangerate-api.com/v6/6c54e1f37780c9f35bb9c009/latest/USD
